31a8548c98a5af5cb0b8efc517035aed9ca1f664
Dani Ren <daniren@Danis-Air-2.hitronhub.home>
2024-01-24 21:56:33 -0800

    Next steps layout

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const dropHandler = function (event) {
 }
 
 /*
-1.) Tasks to be able to be removed
-2.) Dynamic resizing of the main panel 
-3.) Statistics and call to openAI API 
+1.) Tasks to be able to be removed (Dani)
+
+-----------------------------------------
+- Get event handler registering the drop (may have to preventDefault before)
+- once draggable object dragged into remove bin add "hidden" tag to the hrml element 
+-You may want to keep an array of all the tasks and apply a fillter to filter out the hidden ones so they are not calculated in the stats 
+------------------------------------------
+
+2.) Dynamic resizing of the main panel and adding tasks (Michael)
+3.) Calculating the Statistics and displaying them (Dani) 
+4.) Reshuffling the tasks 
+5.) call to OpenAI API 
+6.) Display GPT text 
 (main goals)
 */ 
