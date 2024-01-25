a5d451e8e0ffa50368b9ed6f11611a1861d554b6
Dani Ren <daniren@Danis-Air-2.hitronhub.home>
2024-01-24 21:36:44 -0800

    Create drop handler

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const dropHandler = function (event) {
+    console.log("Heyo");
+    event.preventDefault();
+}
+
+/*
+1.) Tasks to be able to be removed
+2.) Dynamic resizing of the main panel 
+3.) Statistics and call to openAI API 
+(main goals)
+*/ 
